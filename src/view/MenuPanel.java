0634957ba1550faf1b594368c40f8843b5f07b35
DayunJ1410 <ngubachlinh53@gmail.com>
2026-03-31 20:03:40 +0700

    Create MenuPanel.java

---
+++ src/view/MenuPanel.java
@@ -0,0 +1,4 @@
+JPanel menuPanel = new JPanel(new GridLayout(3, 1, 10, 10));
+menuPanel.add(new JButton("Chơi mới"));
+menuPanel.add(new JButton("Bảng xếp hạng"));
+menuPanel.add(new JButton("Thoát"));
