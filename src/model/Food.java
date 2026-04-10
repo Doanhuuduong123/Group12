b9656dea723d6e61ee3f06f689e842754a934f5a
MaiUyen <ngubachlinh53@gmail.com>
2026-04-10 21:04:59 +0700

    sualoi

--- src/model/Food.java
+++ src/model/Food.java
@@ -14,39 +14,72 @@ public class Food extends GameObject {
     }
 
     public void spawn(Snake snake, ArrayList<Wall> walls) {
-        int maxX = 900 / size;
-        int maxY = 600 / size;
+
+        int maxX = 25;
+        int maxY = 15;
+
         boolean validPosition;
         int rx, ry;
 
+        int wallSize = size * 3;
+
         do {
             validPosition = true;
-            rx = rand.nextInt(maxX) * size;
-            ry = rand.nextInt(maxY) * size;
 
+            rx = (rand.nextInt(maxX) + 2) * size;
+            ry = (rand.nextInt(maxY) + 2) * size;
+
+            // rectangle food
+            java.awt.Rectangle foodRect =
+                    new java.awt.Rectangle(rx, ry, size, size);
+
+            // 1. check wall (3x3)
             if (walls != null) {
                 for (Wall w : walls) {
-                    if (rx == w.getX() && ry == w.getY()) {
+
+                    java.awt.Rectangle wallRect =
+                            new java.awt.Rectangle(
+                                    w.getX(),
+                                    w.getY(),
+                                    wallSize,
+                                    wallSize
+                            );
+
+                    if (foodRect.intersects(wallRect)) {
                         validPosition = false;
                         break;
                     }
                 }
             }
 
+            // 2. check snake
             if (validPosition && snake != null && snake.getBody() != null) {
                 for (Point p : snake.getBody()) {
-                    if (rx == p.x && ry == p.y) {
+
+                    java.awt.Rectangle snakeRect =
+                            new java.awt.Rectangle(
+                                    p.x,
+                                    p.y,
+                                    size,
+                                    size
+                            );
+
+                    if (foodRect.intersects(snakeRect)) {
                         validPosition = false;
                         break;
                     }
                 }
             }
+
         } while (!validPosition);
 
         this.x = rx;
         this.y = ry;
+
+        // 20% táo vàng
         this.isSpecial = rand.nextInt(10) > 7;
     }
+
     public boolean isSpecial() {
         return isSpecial;
     }
